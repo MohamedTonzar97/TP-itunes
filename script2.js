477cd5d9ff39a732bcede9fe93cf504424b9d56a
MohamedTonzar97 <32301131+MohamedTonzar97@users.noreply.github.com>
2021-01-27 21:02:41 +0100

    fisrt commit

---
+++ script2.js
@@ -0,0 +1,34 @@
+let button = document.querySelector("#valider");
+let input = document.querySelector("#input");
+let output = document.querySelector("#output");
+
+
+button.addEventListener('click', (e) => {
+    getDataItunes()
+})
+
+
+function getDataItunes() {
+    let url = 'https://itunes.apple.com/search?term='+input.value
+    let cors = 'https://cors-anywhere.herokuapp.com/'
+    fetch(cors+url)
+        .then(data => data.json())
+        .then(json => {
+         console.log(json)
+         let finalHTML = ''
+         json.results.forEach(song => {
+             finalHTML +=
+             ` <div class="card" style="width: 18rem;">
+             <div class="card-body">
+               <img src="${song.artworkUrl100}" class="img-fluid" alt="Responsive image">
+               <h6 class="card-subtitle mb-2 text-muted">${song.artistName}</h6>
+               <p class="card-text">${song.trackCensoredName}</p>
+              
+             </div>
+           </div>`      
+            })
+            output.innerHTML=finalHTML
+        })
+            .catch(error => console.error() )
+
+    }
